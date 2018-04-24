cd4d096e70844581736aefde8f8240fc64f1a84a
VarHug <490641570@qq.com>
2018-04-24 17:43:42 +0800

    路由配置

--- src/main.js
+++ src/main.js
@@ -1,13 +1,33 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
+import VueRouter from 'vue-router';
 import App from './App';
+import goods from './components/goods/goods.vue';
+import ratings from './components/ratings/ratings.vue';
+import seller from './components/seller/seller.vue';
 
 Vue.config.productionTip = false;
+
+Vue.use(VueRouter);
+
+const routes = [
+  {path: '/goods', component: goods},
+  {path: '/ratings', component: ratings},
+  {path: '/seller', component: seller}
+];
+
+const router = new VueRouter({
+  routes,
+  linkActiveClass: 'active'
+});
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   components: { App },
-  template: '<App/>'
+  template: '<App/>',
+  router
 });
+
+router.push('/goods');
