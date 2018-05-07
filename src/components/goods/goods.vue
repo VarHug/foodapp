406a07d358dd785a8ae53c1ff1e001a9cce9a836
VarHug <490641570@qq.com>
2018-05-07 14:59:54 +0800

    menu布局

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -1,11 +1,87 @@
 <template>
-  <div>I am goods</div>
+  <div class="goods">
+    <div class="menu-wrapper">
+      <ul>
+        <li v-for="item in goods" :key="item.id" class="menu-item">
+          <span class="text border-1px">
+            <span v-show="item.type>0" class="icon" :class="classMap[item.type]"></span>{{item.name}}
+          </span>
+        </li>
+      </ul>
+    </div>
+    <div class="foods-wrapper"></div>
+  </div>
 </template>
 
 <script type="text/ecmascript-6">
-export default {};
+const ERR_OK = 0;
+
+export default {
+  props: {
+    seller: {
+      type: Object
+    }
+  },
+  data() {
+    return {
+      goods: []
+    };
+  },
+  created() {
+    this.classMap = ['decrease', 'discount', 'special', 'invoice', 'guarantee'];
+    this.$axios.get('/api/goods').then((response) => {
+        if (response.data.errno === ERR_OK) {
+          this.goods = response.data.data;
+        }
+      });
+  }
+};
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
+  @import "../../common/stylus/mixin";
 
+  .goods
+    display flex
+    position absolute
+    top 174px
+    bottom 46px
+    width 100%
+    overflow hidden
+    .menu-wrapper
+      flex 0 0 80px
+      width 80px
+      background #f3f5f7
+      .menu-item
+        display table
+        height 54px
+        width 56px
+        line-height 14px
+        padding 0 12px
+        .text
+          display table-cell
+          width 56px
+          vertical-align middle
+          border-1px(rgba(7, 17, 27, 0.1))
+          font-size 12px
+          .icon
+            display inline-block
+            vertical-align top
+            width 12px
+            height 12px
+            margin-right 2px
+            background-size 12px 12px
+            background-repeat no-repeat
+            &.decrease
+              bg-image('decrease_3')
+            &.discount
+              bg-image('discount_3')
+            &.guarantee
+              bg-image('guarantee_3')
+            &.invoice
+              bg-image('invoice_3')
+            &.special
+              bg-image('special_3')
+    .foods-wrapper
+      flex 1
 </style>
