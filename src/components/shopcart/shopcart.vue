1cbac81d3c5f23bf3ef0a69f07eb30db61253af4
VarHug <490641570@qq.com>
2018-05-13 16:16:31 +0800

    购物车详情页

--- src/components/shopcart/shopcart.vue
+++ src/components/shopcart/shopcart.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="shopcart">
     <div class="content">
-      <div class="content-left">
+      <div class="content-left" @click="toggleList">
         <div class="logo-wrapper">
           <div class="logo" :class="{'highlight': totalCount>0}">
             <i class="icon-shopping_cart"></i>
@@ -11,7 +11,7 @@
         <div class="price" :class="{'highlight': totalPrice>0}">¥{{totalPrice}}</div>
         <div class="desc">另需配送费¥{{deliveryPrice}}元</div>
       </div>
-      <div class="content-right">
+      <div class="content-right" @click="pay">
         <div class="pay" :class="{'highlight': totalPrice>=minPrice}">{{payDesc}}</div>
       </div>
     </div>
@@ -28,10 +28,37 @@
       </div>
       </transition-group>
     </div>
+    <transition name="fold">
+    <div class="shopcart-list" v-show="listShow">
+      <div class="list-header">
+        <h1 class="title">购物车</h1>
+        <span class="empty" @click="empty">清空</span>
+      </div>
+      <div class="list-content" ref="listContent">
+        <ul>
+          <li class="food" v-for="(food, index) in selectFoods" :key="index">
+            <span class="name">{{food.name}}</span>
+            <div class="price">
+              <span>¥{{food.price*food.count}}</span>
+            </div>
+            <div class="cartcontrol-wrapper">
+              <cartcontrol :food="food"></cartcontrol>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
+    </transition>
+    <transition name="fade">
+    <div class="list-mask" v-show="listShow" @click="hideList"></div>
+    </transition>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
+import BScroll from 'better-scroll';
+import cartcontrol from '../cartconcontrol/cartconcontrol';
+
 export default {
   props: {
     selectFoods: {
@@ -68,7 +95,8 @@ export default {
           show: false
         }
       ],
-      dropBalls: []
+      dropBalls: [],
+      fold: true
     };
   },
   computed: {
@@ -96,6 +124,48 @@ export default {
         return '还差' + diff + '元起送';
       } else {
         return '去结算';
+      }
+    },
+    listShow() {
+      return !this.fold;
+    }
+    // ,
+    // listShow() {
+    //   if (!this.totalCount) {
+    //     this.fold = true;
+    //     return false;
+    //   }
+    //   let show = !this.fold;
+    //   if (show) {
+    //     this.$nextTick(() => {
+    //       if (!this.scroll) {
+    //         this.scroll = new BScroll(this.$refs.listContent, {
+    //           click: true
+    //         });
+    //       } else {
+    //         this.scroll.refresh();
+    //       }
+    //   });
+    //   }
+    //   return show;
+    // }
+  },
+  watch: {
+    totalCount: function () {
+      if (!this.totalCount) {
+        this.fold = true;
+      }
+      let show = !this.fold;
+      if (show) {
+        this.$nextTick(() => {
+          if (!this.scroll) {
+            this.scroll = new BScroll(this.$refs.listContent, {
+              click: true
+            });
+          } else {
+            this.scroll.refresh();
+          }
+        });
       }
     }
   },
@@ -145,12 +215,37 @@ export default {
         ball.show = false;
         el.style.display = 'none';
       }
+    },
+    toggleList() {
+      if (!this.totalCount) {
+        return;
+      }
+      this.fold = !this.fold;
+    },
+    empty() {
+      this.selectFoods.forEach((food) => {
+        food.count = 0;
+      });
+    },
+    hideList() {
+      this.fold = true;
+    },
+    pay() {
+      if (this.totalPrice < this.minPrice) {
+        return;
+      }
+      window.alert('支付' + this.totalPrice + '元');
     }
+  },
+  components: {
+    cartcontrol
   }
 };
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
+  @import "../../common/stylus/mixin";
+
   .shopcart
     position fixed
     left 0
@@ -247,9 +342,81 @@ export default {
           height 16px
           border-radius 50%
           background-color rgb(0, 160, 220)
+    .shopcart-list
+      position absolute
+      top 0
+      left 0
+      transform translate3d(0, -100%, 0)
+      z-index -1
+      width 100%
+      // &.fold-enter-active, &.fold-leave-active
+      //   transition all .5s
+      // &.fold-enter, &.fold-leave-to
+      //   transform translate3d(0, 0, 0)
+      .list-header
+        line-height 40px
+        padding 0 18px
+        height 40px
+        background-color #f3f5f7
+        border-bottom 1px solid rgba(7, 17, 27, 0.1)
+        .title
+          float left
+          font-size 14px
+          color rgb(7, 17, 27)
+        .empty
+          float right
+          font-size 12px
+          color rgb(0, 160, 220)
+      .list-content
+        padding 0 18px
+        max-height 217px
+        overflow hidden
+        background-color #fff
+        .food
+          position relative
+          padding 12px 0
+          box-sizing border-box
+          border-1px(rgba(7, 17, 27, 0.1))
+          .name
+            line-height 24px
+            font-size 14px
+            color rgb(7, 17, 27)
+          .price
+            position absolute
+            right 90px
+            bottom 12px
+            line-height 24px
+            font-size 1px
+            font-weight 700
+            color rgb(240, 20, 20)
+          .cartcontrol-wrapper
+            position absolute
+            right 0
+            bottom 6px
+    .list-mask
+      position fixed
+      top 0
+      left 0
+      width 100%
+      height 100%
+      z-index -2
+      background rgba(7, 17, 27, 0.6)
+      backdrop-filter blur(10px)
 
   .drop-enter-active
     transition all .4s cubic-bezier(0.49, -0.29, 0.75, 0.41)
     .inner
       transition all .4s linear
+
+  .fold-enter-active, .fold-leave-active
+    transition all .5s
+  .fold-enter, .fold-leave-to
+    transform translate3d(0, 0, 0) !important
+
+  .fade-enter-active, .fade-leave-active
+    opacity 1
+    transition all .5s
+  .fade-enter, .fade-leave-to
+    opacity 0
+    background rgba(7, 17, 27, 0)
 </style>
