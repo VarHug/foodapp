5f7b6c4fb9591e3583a27aaf14014a05a1e9c3df
VarHug <490641570@qq.com>
2018-05-22 15:28:00 +0800

    finish ratingselect component #1

--- src/components/ratingselect/ratingselect.vue
+++ src/components/ratingselect/ratingselect.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="ratingselect">
     <div class="rating-type border-1px">
-      <span class="block positive" :class="{'active':selectType===2}">{{desc.all}}<span class="count">47</span></span>
-      <span class="block positive" :class="{'active':selectType===0}">{{desc.positive}}<span class="count">47</span></span>
-      <span class="block negative" :class="{'active':selectType===1}">{{desc.negative}}<span class="count">47</span></span>
+      <span class="block positive" :class="{'active':selectType===2}" @click="select(2, $event)">{{desc.all}}<span class="count">{{ratings.length}}</span></span>
+      <span class="block positive" :class="{'active':selectType===0}" @click="select(0, $event)">{{desc.positive}}<span class="count">{{positives.length}}</span></span>
+      <span class="block negative" :class="{'active':selectType===1}" @click="select(1, $event)">{{desc.negative}}<span class="count">{{negatives.length}}</span></span>
     </div>
-    <div class="switch">
+    <div class="switch" :class="{'on':onlyContent}" @click="toggleContent">
       <i class="icon-check_circle"></i>
       <span class="text">只看有内容的评价</span>
     </div>
@@ -13,6 +13,8 @@
 </template>
 
 <script type="text/ecmascript-6">
+import {eventHub} from '../../common/js/eventHub.js';
+
 const POSITIVE = 0;
 const NEGATIVE = 1;
 const ALL = 2;
@@ -48,6 +50,28 @@ export default {
     return {
 
     };
+  },
+  methods: {
+    select(type, event) {
+      this.selectType = type;
+      eventHub.$emit('ratingtype.select', type);
+    },
+    toggleContent(event) {
+      this.onlyContent = !this.onlyContent;
+      eventHub.$emit('content.toggle', this.onlyContent);
+    }
+  },
+  computed: {
+    positives() {
+      return this.ratings.filter((rating) => {
+        return rating.rateType === POSITIVE;
+      });
+    },
+    negatives() {
+      return this.ratings.filter((rating) => {
+        return rating.rateType === NEGATIVE;
+      });
+    }
   },
   components: {
 
@@ -85,4 +109,22 @@ export default {
           background-color rgba(77, 85, 93, 0.2)
           &.active
             background-color rgb(77, 85, 93)
+    .switch
+      padding 12px 18px
+      line-height 24px
+      border-bottom 1px solid rgba(7, 17, 27, 0.1)
+      color rgb(147, 153, 159)
+      font-size 0
+      &.on
+        .icon-check_circle
+          color #00c850
+      .icon-check_circle
+        display inline-block
+        vertical-align top
+        margin-right 4px
+        font-size 24px
+      .text
+        display inline-block
+        vertical-align top
+        font-size 12px
 </style>
