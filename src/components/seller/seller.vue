94945303b03d3353d7a5212a1683eb8dcfee6698
VarHug <490641570@qq.com>
2018-05-24 19:30:17 +0800

    add overview #3

--- src/components/seller/seller.vue
+++ src/components/seller/seller.vue
@@ -1,11 +1,102 @@
 <template>
-  <div>I am seller</div>
+  <div class="seller">
+    <div class="seller-content">
+      <div class="overview">
+        <h1 class="title">{{seller.name}}</h1>
+        <div class="desc border-1px">
+          <star :size="36" :score="seller.score"></star>
+          <span class="text">({{seller.ratingCount}})</span>
+          <span class="text">月售{{seller.sellCount}}单</span>
+        </div>
+        <ul class="remark">
+          <li class="block">
+            <h2>起送价</h2>
+            <div class="content">
+              <span class="stress">{{seller.minPrice}}</span>元
+            </div>
+          </li>
+          <li class="block">
+            <h2>商家配送</h2>
+            <div class="content">
+              <span class="stress">{{seller.deliveryPrice}}</span>元
+            </div>
+          </li>
+          <li class="block">
+            <h2>平均购物时间</h2>
+            <div class="content">
+              <span class="stress">{{seller.deliveryTime}}</span>分钟
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script type="text/ecmascript-6">
-  export default {};
+  import star from '../star/star.vue';
+
+  export default {
+    props: {
+      seller: {
+        type: Object
+      }
+    },
+    components: {
+      star
+    }
+  };
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
+  @import "../../common/stylus/mixin";
 
+  .seller
+    position absolute
+    top 174px
+    left 0
+    bottom 0
+    width 100%
+    overflow hidden
+    .overview
+      padding 18px
+      .title
+        margin-bottom 8px
+        line-height 14px
+        font-size 14px
+        color rgb(7, 17, 27)
+      .desc
+        padding-bottom 18px
+        line-height 18px
+        font-size 0
+        border-1px(rgba(1, 17, 27, 0.1))
+        .star, .text
+          display inline-block
+          vertical-align top
+        .star
+          margin-right 8px
+        .text
+          margin-right 12px
+          font-size 10px
+          color rgb(77, 85, 93)
+      .remark
+        display flex
+        padding-top 18px
+        .block
+          flex 1
+          text-align center
+          border-right 1px solid rgba(7, 17, 27, 0.1)
+          &:last-child
+            border none
+          h2
+            margin-bottom 4px
+            line-height 10px
+            font-size 10px
+            color rgb(147, 153, 159)
+          .content
+            line-height 24px
+            font-size 10px
+            color rgb(7, 17, 27)
+            .stress
+              font-size 24px
 </style>
